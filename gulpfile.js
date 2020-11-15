839430933077fdbf67a1654a85b42e45b381a3d9
Kasper <kasper_klinke@hotmail.com>
2020-11-15 06:37:27 +0100

    oprettet gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const terser = require("gulp-terser");
+const rename = require("gulp-rename");
+
+function styles() {
+    return(
+        gulp.src("css/*.scss")
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            .pipe(sourcemaps.write("."))
+            .pipe(gulp.dest("css"))
+    );
+}
+
+function javascript() {
+    return(
+        gulp.src(["js/*.js", "!js/*min.js"])
+            .pipe(sourcemaps.init())
+            .pipe(terser())
+            .pipe(rename({
+                suffix:".min"
+            }))
+            .pipe(sourcemaps.write("."))
+            .pipe(gulp.dest("js"))
+
+    );
+}
+
+function watch(){
+    gulp.watch("css/*.scss", styles);
+    gulp.watch(["js/*.js", "!js/*min.js"], javascript);
+
+}
+
+exports.styles = styles;
+exports.javascript = javascript;
+exports.watch = watch;
